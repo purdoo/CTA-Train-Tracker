4b649c39d08dab76fe74577153b928e8cd36715f
Chris <chrisliow@gmail.com>
2016-07-26 11:35:30 -0500

    tip-toeing around the asynch calls

--- CTA/js/cta.js
+++ CTA/js/cta.js
@@ -1,11 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
   console.log('loaded');
   initLineSelector();
+  var parentMapping = [];
+  $('#line-select').on('change', function() {
+    console.log($(this).val());
+    mapping = initStopSelector($(this).val());
+  });
 
   var api_key = '4ffee39f79e54e19b75756aded7cb3d3';
   var arrivals_base_url = 'http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx/';
   //var test_station_id = '30062';
   $('#connect').click(function() {
+    console.log(parentMapping);
     var request_url = arrivals_base_url + '?key=' + api_key;
     request_url += '&stpid=' + $('#stop-select').val();
     console.log(request_url);
@@ -26,58 +32,58 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 
-  $('#line-select').on('change', function() {
-    console.log($(this).val());
-    initStopSelector($(this).val());
-  });
-
-}, false);
-
-function initLineSelector() {
-  var lines = {
-    "Select a Line":"-",
-    "Red Line":"RED",
-    "Blue Line":"BLUE",
-    "Green Line":"G",
-    "Brown Line":"BRN",
-    "Purple Line (Non Express)":"P",
-    "Yellow Line":"Y",
-    "Pink Line (Non Express)":"Pnk",
-    "Orange Line":"O"
-  };
-  for(var line in lines) {
-    $('<option value="'+ lines[line] + '">'+ line + '</option>').appendTo('#line-select');
+  function initLineSelector() {
+    var lines = {
+      "Select a Line":"-",
+      "Red Line":"RED",
+      "Blue Line":"BLUE",
+      "Green Line":"G",
+      "Brown Line":"BRN",
+      "Purple Line (Non Express)":"P",
+      "Yellow Line":"Y",
+      "Pink Line (Non Express)":"Pnk",
+      "Orange Line":"O"
+    };
+    for(var line in lines) {
+      $('<option value="'+ lines[line] + '">'+ line + '</option>').appendTo('#line-select');
+    }
   }
-}
 
-function initStopSelector(lineId) {
-  $('#stop-select').find('option').remove().end();
-  var rawFile = new XMLHttpRequest();
-  rawFile.open('GET', 'files/stops.csv', false);
-  rawFile.onreadystatechange = function ()
-  {
-    if(rawFile.readyState === 4)
-    {
-      if(rawFile.status === 200 || rawFile.status == 0)
-      {
-        console.log(lineId);
-        var allText = rawFile.responseText;
-        var csvResults = CSVToArray(allText, ',')
-        var stopIndex = processHeader(csvResults[0], lineId);
-        var csvResults = csvResults.slice(1);
-        for(var x in csvResults) {
-          if(csvResults.hasOwnProperty(x)) {
-            if(csvResults[x][stopIndex] == 'TRUE') {
-              $('<option value="'+ csvResults[x][0] + '">'+ csvResults[x][2] + '</option>').appendTo('#stop-select');
+  function initStopSelector(lineId) {
+    $('#stop-select').find('option').remove().end();
+    var rawFile = new XMLHttpRequest();
+    rawFile.open('GET', 'files/stops.csv', false);
+    rawFile.onreadystatechange = function () {
+      if(rawFile.readyState === 4) {
+        if(rawFile.status === 200 || rawFile.status == 0) {
+          console.log(lineId);
+          var allText = rawFile.responseText;
+          var csvResults = CSVToArray(allText, ',')
+          var stopIndex = processHeader(csvResults[0], lineId);
+          var csvResults = csvResults.slice(1);
+          for(var x in csvResults) {
+            if(csvResults.hasOwnProperty(x)) {
+              if(csvResults[x][stopIndex] == 'TRUE') {
+                var mapId = csvResults[x][5];
+                if(mapId in parentMapping) {
+                  parentMapping[mapId].push(csvResults[x][0]);
+                  $('<option value="'+ mapId + '">'+ csvResults[x][3] + '</option>').appendTo('#stop-select');
+                }
+                else {
+                  parentMapping[mapId] = [csvResults[x][0]];
+                }
+              }
             }
           }
         }
       }
-    }
+    } // end asynch function
+    rawFile.send(null);
   }
-  rawFile.send(null);
 
-}
+}, false);
+
+
 
 function formatTime(timeString) {
   var split = timeString.split(':');
